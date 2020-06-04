5b0d13ab491c22e19bc0ab734348b7ae99b744b7
Frederico batista <fred@penumbra.design>
2020-06-04 18:21:20 -0300

    omg

--- 404.php
+++ 404.php
@@ -1,22 +1,5 @@
 <?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package estudio-triciclo
- */
-
-get_header();
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: ".get_bloginfo('url'));
+exit();
 ?>
-
-		<?php
-
-
-      the_post(get_option('page_on_front'));
-
-			get_template_part( 'content-templates/content-'.get_post_type(), get_post_field( 'post_name', get_post() )
-					 );
-		?>
-<?php
-get_footer();
