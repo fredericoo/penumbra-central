bb05b594cf96f42625ae54ef6ae597179933b320
Frederico Batista <frederico@Fredericos-MacBook-Pro.local>
2020-06-19 18:05:41 -0300

    fix to modal not working when product out of stock

--- js/ajax.js
+++ js/ajax.js
@@ -95,7 +95,8 @@ jQuery('#modal').on('show.bs.modal', function (event) {
   })
   modal.querySelector('.produto__imagem>img').setAttribute('src',product.getAttribute('data-fullimage'));
   modal.querySelector('.produto__imagem>img').className = "lazyload fade"
-  modal.querySelector('.produto__addtocart').appendChild(lastClickedProduct.querySelector('.cart'));
+
+  modal.querySelector('.produto__addtocart').appendChild(lastClickedProduct.querySelector('.produto__addtocart>*'));
 
   lazyLoadInstance.update();
   scrollPop.updatePoppable();
@@ -108,7 +109,7 @@ jQuery('#modal').on('show.bs.modal', function (event) {
 jQuery('#modal').on('hidden.bs.modal', function (event) {
   if (lastClickedProduct) {
     console.log(lastClickedProduct)
-    lastClickedProduct.querySelector('.produto__addtocart').appendChild(this.querySelector('.cart'));
+    lastClickedProduct.querySelector('.produto__addtocart').appendChild(this.querySelector('.produto__addtocart>*'));
   }
 });
 
